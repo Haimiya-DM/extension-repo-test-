a446533e9ad6d7ef84421f29bf64fe67ed79d19c
Haimiya-DM <azuna.dqrk@gmail.com>
2026-09-12 00:19:14 +0800

    Add/update extension source: build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    // Root build script plugins
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://www.jitpack.io")
+    }
+}
