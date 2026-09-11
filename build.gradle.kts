7eb37840672df1f5f1300a79aa3899d747055c45
Haimiya-DM <azuna.dqrk@gmail.com>
2026-09-12 00:38:44 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
     // Root build script plugins
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://www.jitpack.io")
-    }
-}
